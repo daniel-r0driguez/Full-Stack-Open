89d6053404b8b870118d6be4e6d0a187b8f4dd4a
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-07-12 20:46:44 -0700

    Finished exercise 1.11*
    Final modification for the unicafe project in Part 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,14 @@ const Header = ({headerText}) => <h1>{headerText}</h1>;
 
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>;
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>;
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({goodClicks, neutralClicks, badClicks}) => {
   const total = goodClicks + neutralClicks + badClicks;
@@ -22,12 +29,16 @@ const Statistics = ({goodClicks, neutralClicks, badClicks}) => {
   return (
     <>
       <h2>Statistics</h2>
-      <StatisticLine text='Good:' value={goodClicks}/>
-      <StatisticLine text='Neutral:' value={neutralClicks}/>
-      <StatisticLine text='Bad:' value={badClicks}/>
-      <StatisticLine text='All:' value={total}/>
-      <StatisticLine text='Average:' value={average}/>
-      <StatisticLine text='Positive:' value={positivePercentage}/>
+      <table>
+        <tbody>
+          <StatisticLine text='Good:' value={goodClicks}/>
+          <StatisticLine text='Neutral:' value={neutralClicks}/>
+          <StatisticLine text='Bad:' value={badClicks}/>
+          <StatisticLine text='All:' value={total}/>
+          <StatisticLine text='Average:' value={average}/>
+          <StatisticLine text='Positive:' value={positivePercentage}/>
+        </tbody>
+      </table>
     </>
   )
 }
