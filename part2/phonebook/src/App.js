f70be2738b372da678d14f5ab38bf3260ecea04b
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-07-17 14:15:14 -0700

    Finished exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,43 @@
 import { useState } from 'react';
 
 const App = () => {
+  // Set the states.
   const [persons, setPersons] = useState([
     {name: 'Arto Hellas'}
   ]);
 
   const [newName, setNewName] = useState('');
 
+  /**
+   * Checks to see if a name is already in the persons array.
+   * @param {string} _newName the potential new name to add to the array.
+   * @returns true if the name is already added, false otherwise
+   */
+  const checkIfAdded = (_newName) => {
+    const added = persons.find((person) => {
+      return person.name === _newName;
+    });
+
+    // If an index is found, then that means it has already been added to the array. Return true.
+    if (added !== undefined)
+    {
+      alert(`${_newName} is already added to phonebook`);
+      return true;
+    }
+    return false;
+  }
+
   const addPerson = (event) => {
     event.preventDefault();
+
+    // Check if the name is already in the array.
+    if (checkIfAdded(newName))
+    {
+      // If so, do not add it. Simply return.
+      return;
+    }
+    
+    // If it is a new name, create a new person object and update the states.
     const newPerson = {
       name: newName
     };
