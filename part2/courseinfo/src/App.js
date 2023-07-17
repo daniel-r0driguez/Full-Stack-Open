f1d7862b09121cfec71be11720e086a0b5f61170
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-07-16 20:07:15 -0700

    Finished exercises 2.2 and 2.3*

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -3,7 +3,9 @@ import Course from './components/Course';
 const App = () => {
   const course = {
     id: 1,
+
     name: 'Half Stack application development',
+
     parts: [
       {
         name: 'Fundamentals of React',
@@ -19,8 +21,19 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Test Course',
+        exercises: 15,
+        id: 4
       }
-    ]
+    ],
+
+    getTotal: function() {
+      return this.parts.reduce((sum, part) => {
+        return sum + part.exercises;
+      }, 0);
+    }
   }
 
   return <Course course={course} />
--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Content from './Content';
+import Total from './Total';
 
 const Course = ({course}) => {
     return (
         <div>
-            <Header text='Half Stack application development'/>
+            <Header text={course.name}/>
             <Content parts={course.parts}/>
+            <Total total={course.getTotal()}/>
         </div>
     );
 };
