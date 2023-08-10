eef21a07a0c7498bd0cdb25e696488b7d7c44dba
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-08-09 18:12:52 -0700

    Added frontend production build to backend project

--- part3/lecture-app/index.js
+++ part3/lecture-app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 let notes = [
@@ -32,6 +33,8 @@ const requestLogger = (request, response, next) => {
 }
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static('build'));
 
 const generateID = () => {
     const maxID = notes.length > 0
@@ -87,7 +90,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end();
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
